c6a9858e677abaa892b87452f85e3d57de7925af
madliani <unixsocket@hotmail.com>
2026-07-27 19:32:33 +0400

    feat(server): add Invoke

---
+++ server/tasks.py
@@ -0,0 +1,45 @@
+from invoke import task
+
+# Helpers
+
+
+class UVHelper:
+    """uv helper."""
+
+    uv_cmd = "uv"
+
+    def run(self, cmd: str) -> str:
+        """Run command in virtual environment."""
+
+        run_cmd = "run"
+
+        return f"{self.uv_cmd} {run_cmd} {cmd}"
+
+
+class FlaskHelper:
+    """Flask helper."""
+
+    flask_cmd = "flask"
+
+    def run(self, app_path: str) -> str:
+        """Run Flask server."""
+
+        run_cmd = "run"
+
+        return f"{self.flask_cmd} --app {app_path} {run_cmd}"
+
+
+uv = UVHelper()
+flask = FlaskHelper()
+
+
+# Tasks
+
+
+@task
+def run(cmd):
+    """Task for server running."""
+
+    app_path = "./app/app.py"
+
+    cmd.run(uv.run(flask.run(app_path)))
