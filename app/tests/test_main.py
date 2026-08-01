f51b57fdded5ac75f64b82b7d04d1a797066724b
madliani <unixsocket@hotmail.com>
2026-08-01 21:46:38 +0400

    feat: add health route test

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -7,3 +7,9 @@ def test_index(test_client):
     response = test_client.get("/")
 
     assert loads(response.data.decode()) == {"status": Status.Ok}
+
+
+def test_health(test_client):
+    response = test_client.get("/")
+
+    assert loads(response.data.decode()) == {"status": Status.Ok}
