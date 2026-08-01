a5c7930a75d930f8b4bd50e222a90f7119816cd4
madliani <unixsocket@hotmail.com>
2026-08-01 18:04:36 +0400

    feat: rename Post to PostCard and make migration

--- app/main.py
+++ app/main.py
@@ -18,8 +18,8 @@ migrate = Migrate(app=app, db=db)
 
 
 # ty:ignore[unsupported-base]
-class Post(db.Model):
-    __tablename__ = "posts"
+class PostCard(db.Model):
+    __tablename__ = "post_cards"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(unique=False, nullable=False)
