6a416e3231dba92afc26ba93005b0e3f6e0b5f58
madliani <unixsocket@hotmail.com>
2026-08-02 17:55:38 +0400

    refactor: refactor main tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -6,12 +6,14 @@ from app.main import Status
 def test_index(test_client):
     response = test_client.get("/")
     data = response.data.decode()
+    json_data = loads(data)
 
-    assert loads(data) == {"status": Status.Ok}
+    assert json_data == {"status": Status.Ok}
 
 
 def test_health(test_client):
     response = test_client.get("/")
     data = response.data.decode()
+    json_data = loads(data)
 
-    assert loads(data) == {"status": Status.Ok}
+    assert json_data == {"status": Status.Ok}
