dbabf0304d7caa8bee5db273c56bf9412f7e34d3
madliani <unixsocket@hotmail.com>
2026-08-02 23:08:17 +0400

    refactor: rename CardModel to Card

--- server/main.py
+++ server/main.py
@@ -15,7 +15,7 @@ migrate = Migrate(app=app, db=db)
 
 
 # ty:ignore[unsupported-base]
-class CardModel(db.Model):
+class Card(db.Model):
     __tablename__ = "cards"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -27,22 +27,22 @@ class CardRepository:
     def __init__(self, db: SQLAlchemy):
         self.db = db
 
-    def get_cards(self) -> list[CardModel]:
-        return self.db.session.query(CardModel).all()
+    def get_cards(self) -> list[Card]:
+        return self.db.session.query(Card).all()
 
-    def add_card(self, title: str, desc: str) -> CardModel:
-        card = CardModel(title=title, desc=desc)
+    def add_card(self, title: str, desc: str) -> Card:
+        card = Card(title=title, desc=desc)
 
         self.db.session.add(card)
         self.db.session.commit()
 
         return card
 
-    def get_card(self, id: int) -> CardModel | None:
-        return self.db.session.query(CardModel).get(id)
+    def get_card(self, id: int) -> Card | None:
+        return self.db.session.query(Card).get(id)
 
-    def update_card(self, id: int, title: str, desc: str) -> CardModel | None:
-        card = self.db.session.query(CardModel).get(id)
+    def update_card(self, id: int, title: str, desc: str) -> Card | None:
+        card = self.db.session.query(Card).get(id)
 
         if card is None:
             return None
@@ -54,8 +54,8 @@ class CardRepository:
 
         return card
 
-    def delete_card(self, id: int) -> CardModel | None:
-        card = self.db.session.query(CardModel).get(id)
+    def delete_card(self, id: int) -> Card | None:
+        card = self.db.session.query(Card).get(id)
 
         if card is None:
             return None
@@ -67,7 +67,7 @@ class CardRepository:
 
 
 class OneCardSerializer:
-    def __init__(self, card: CardModel):
+    def __init__(self, card: Card):
         self.card = card
 
     def serialize(self):
@@ -79,7 +79,7 @@ class OneCardSerializer:
 
 
 class ManyCardSerializer:
-    def __init__(self, cards: list[CardModel]):
+    def __init__(self, cards: list[Card]):
         self.cards = cards
 
     def serialize(self):
